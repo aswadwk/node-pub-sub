0bb334ec2294c8a2de396c934a9da5dad0dbee21
aswadwk <hajaraswadkom@gmail.com>
2025-05-03 12:53:12 +0700

    refactor: add KEY_SECRETS handling and sanitizeSecrets function to mask sensitive data in requests

--- src/controllers/pub-sub-controller.js
+++ src/controllers/pub-sub-controller.js
@@ -3,11 +3,35 @@ import rabbitMQService from '../services/rabbit-mq-service.js';
 import { publishMessageValidation } from '../validations/auth-validation.js';
 import validate from '../validations/validation.js';
 
+const KEY_SECRETS = process.env.KEY_SECRETS ? process.env.KEY_SECRETS.split(',') : [
+    'x-api-key',
+    'password',
+];
+
+const sanitizeSecrets = (obj, secrets) => {
+    if (!obj || typeof obj !== 'object') return obj;
+
+    const sanitized = { ...obj };
+
+    for (const key in sanitized) {
+        if (secrets.includes(key)) {
+            sanitized[key] = '********';
+        } else if (typeof sanitized[key] === 'object') {
+            sanitized[key] = sanitizeSecrets(sanitized[key], secrets);
+        }
+    }
+
+    return sanitized;
+};
+
+
 const publishMessage = async (req, res, next) => {
     try {
         const validatePublishMessage = validate(publishMessageValidation, req.body);
 
-        logger.info('Incoming request to publish message:', { body: req.body });
+        const sanitizedBody = sanitizeSecrets(req.body, KEY_SECRETS);
+
+        logger.info('Incoming request to publish message:', { body: sanitizedBody });
 
         await rabbitMQService.publish(validatePublishMessage, 'delayed_exchange', 'push-notification', req.body?.delay);
 
